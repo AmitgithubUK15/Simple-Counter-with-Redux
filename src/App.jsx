5d7edb252d65fabb98064f3ee9cc697f642d022f
Amit <Amitshah050112003@gmail.com>
2024-02-19 18:24:07 +0530

    action,payload sai userinput ui mai show karna

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
 import {useSelector,useDispatch} from 'react-redux';
-import { increament ,decreament} from './features/fun/counterSlice';
+import { increament ,decreament,userbio} from './features/fun/counterSlice';
+import { useRef } from 'react';
 
 function App() {
- 
+  
+  const userinput = useRef()
   const count = useSelector(state => state.counter);
   const increby10 = useSelector(state => state.increby10);
- 
+  const showbio = useSelector(state => state.showdata)
+  
   const dispatch = useDispatch();
   
+  function handlesubmit(e){
+    e.preventDefault();
+
+    dispatch(userbio(userinput.current.value));
+  }
   return (
     <>
      <h1>this counter create help with React-redux</h1>
@@ -23,6 +31,14 @@ function App() {
      onClick={()=>dispatch(decreament())}
      >increament</button>
     <h1>{increby10}</h1>
+
+     <form onSubmit={handlesubmit}>
+     <h1>Enter your name</h1>
+      <input type="text" ref={userinput} />
+      <button type='submit'>Get</button>
+     </form>
+
+     <h2>{showbio}</h2>
     </>
   )
 }
--- src/features/fun/counterSlice.js
+++ src/features/fun/counterSlice.js
@@ -2,7 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
    counter:0,
-   increby10:0
+   increby10:0,
+   showdata:"",
 }
 
 export const counteraction = createSlice({
@@ -16,9 +17,15 @@ export const counteraction = createSlice({
         decreament:(state)=>{
             state.counter -=1,
             state.increby10 -=10
+        },
+        userbio:(state,action) =>{
+            const text = {
+                val:action.payload
+            }
+            state.showdata = `hii, my name is ${text.val}`;
         }
     }
 })
 
-export const {increament,decreament} = counteraction.actions
+export const {increament,decreament , userbio} = counteraction.actions
 export default counteraction.reducer
